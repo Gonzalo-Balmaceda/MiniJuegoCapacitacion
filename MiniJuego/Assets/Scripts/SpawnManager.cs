71bad6e7efcfe994de58e17112dae12ba8d92a0f
Gonzalo Balmaceda <www.gonzalobalmaceda@gmail.com>
2024-11-30 20:06:38 -0300

    Agrego nuevos items
    He agregado tres nuevos items para la suma de puntos, resta de vida y uno que dará un boost de velocidad al jugador

--- MiniJuego/Assets/Scripts/SpawnManager.cs
+++ MiniJuego/Assets/Scripts/SpawnManager.cs
@@ -7,16 +7,19 @@ public class SpawnManager : MonoBehaviour
 {
     private PlayerController playerControllerScript;
     public GameObject[] itemsPrefabs;
+    public GameObject[] clockPrefab;
     private float spawnRangeX = 10.2F; // Rango de spawn en el eje x.
     private float startDelay = 3F;
-    private float spawnInterval = 1.5F;
+    private float spawnInterval = 1F;
+    private float spawnIntervalClock = 30F;
 
     // Start is called before the first frame update
     void Start()
     {
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
         InvokeRepeating("SpawnRandomItems", startDelay, spawnInterval);
-        
+        InvokeRepeating("WaitForSpawnClock", startDelay, spawnIntervalClock);
+
     }
 
     // Update is called once per frame
@@ -25,13 +28,20 @@ public class SpawnManager : MonoBehaviour
 
     }
 
-    void SpawnRandomItems() 
+    void SpawnRandomItems()
     {
         if (playerControllerScript.gameOver == false)
         {
             int itemIndex = UnityEngine.Random.Range(0, itemsPrefabs.Length); // Genero un número al azar entre o y el largo del arreglo.
             Vector2 spawnPos = new Vector2(UnityEngine.Random.Range(-spawnRangeX, spawnRangeX), 5.22F); // Limites en donde spwnearan los items.
-            Instantiate(itemsPrefabs[itemIndex],spawnPos, itemsPrefabs[itemIndex].transform.rotation); // Generamos los itemes aleatoriamente. 
+            Instantiate(itemsPrefabs[itemIndex], spawnPos, itemsPrefabs[itemIndex].transform.rotation); // Generamos los itemes aleatoriamente. 
         }
+    }
+
+    void WaitForSpawnClock()
+    {
+        int itemIndex = 0;
+        Vector2 spawnPos = new Vector2(UnityEngine.Random.Range(-spawnRangeX, spawnRangeX), 5.22F); // Limites en donde spwnearan los items.
+        Instantiate(clockPrefab[itemIndex], spawnPos, clockPrefab[itemIndex].transform.rotation); // Generamos los itemes aleatoriamente. 
     }
 }
